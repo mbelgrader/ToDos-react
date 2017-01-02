4d048434c2bd7bead87c1685112463122d7167f3
mbelgrader <mbelgrader5@gmail.com>
2017-01-02 13:43:16 -0800

    Add Redux Store

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,7 +1,22 @@
 import merge from 'lodash/merge';
-import { RECEIVE_TODOS, RECEIVE_TODO } from './actions/todo_actions';
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_TODOS:
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store.js';
+
+const store = configureStore();
+window.store = store;
 
 class Todos extends React.Component {
   render() {
