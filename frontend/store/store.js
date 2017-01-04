af52e8b9dda071a89cbf50f3541cf77aa22a417c
mbelgrader <mbelgrader5@gmail.com>
2017-01-03 17:13:56 -0800

    Add thunk middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+import thunk from '../middleware/thunk';
 
-const configureStore = () => (createStore(rootReducer));
+const configureStore = () => (createStore(rootReducer, applyMiddleware(thunk)));
 
 export default configureStore;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -5,6 +5,7 @@ import { receiveTodos, receiveTodo} from './actions/todo_actions.js';
 import App from './components/app';
 import Root from './components/root';
 import configureStore from './store/store';
+import { fetchTodos } from './util/todo_api_util';
 
 document.addEventListener("DOMContentLoaded", function(){
   // window.store = configureStore();
@@ -13,5 +14,6 @@ document.addEventListener("DOMContentLoaded", function(){
   // window.receiveTodos = receiveTodos;
   const store = configureStore();
   window.store = store;
+  window.fetchTodos = fetchTodos;
   ReactDOM.render(<Root store={store} />, document.getElementById("content"));
 });
